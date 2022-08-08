e11c7a4a664bf1c5ccc4fdc0d55005646fd3d485
AndrewPorto <andrewluizporto@hotmail.com>
2022-08-08 16:31:21 -0300

    projeto iniciado

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Acordes Maiores
+
+function playc() {
+    var audio = document.getElementById("audio-c");
+    audio.play();
+  }
+ 
+  function playd() {
+    var audio = document.getElementById("audio-d");
+    audio.play();
+  }
+
+  function playe() {
+    var audio = document.getElementById("audio-e");
+    audio.play();
+  }
+
+  function playf() {
+    var audio = document.getElementById("audio-f");
+    audio.play();
+  }
+
+  function playg() {
+    var audio = document.getElementById("audio-g");
+    audio.play();
+  }
+
+  function playa() {
+    var audio = document.getElementById("audio-a");
+    audio.play();
+  }
+
+  function playb() {
+    var audio = document.getElementById("audio-b");
+    audio.play();
+  }
+
+  // Acordes menores 7+
+
+  function playcm7M() {
+    var audio = document.getElementById("audio-cm7M");
+    audio.play();
+  }
+
+  function playdm7M() {
+    var audio = document.getElementById("audio-dm7M");
+    audio.play();
+  }
+
+  function playem7M() {
+    var audio = document.getElementById("audio-em7M");
+    audio.play();
+  }
